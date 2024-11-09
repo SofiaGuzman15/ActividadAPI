c31dab22cef2309b2dece273549e3e52918b54d8
SofiaGuzman15 <lalitacruz4@gmail.com>
2024-11-08 19:19:00 -0500

    Add files via upload

---
+++ Backend/Actividad.API/Controllers/Employees.Controller.cs
@@ -0,0 +1,51 @@
+﻿using Actividad.API.Models;
+using Actividad.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Crypto;
+
+namespace Actividad.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeesService _employeesService;
+
+        public EmployeesController(EmployeesService EmployeesService)
+        {
+            _employeesService = EmployeesService;
+        }
+
+        [HttpPost("Registrar")]
+        public IActionResult RegistrarEmployees([FromBody] Employees employees)
+        {
+            try
+            {
+                _employeesService.RegistrarEmployee(employees);
+                return Ok(new
+                {
+                    message = "Empleado registrado"
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.ToString());
+            }
+        }
+
+        [HttpGet("GetEmployees")]
+        public ActionResult<IEnumerable<Employees>> GetEmployees()
+        {
+            try
+            {
+                var employees = _employeesService.GetEmployees();
+                return Ok(employees);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Backend/Actividad.API/Services/Employees.Service.cs
@@ -0,0 +1,28 @@
+﻿using Actividad.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace Actividad.API.Services
+{
+    public class EmployeesService
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesService(AppDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public IEnumerable<Employees> GetEmployees()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public void RegistrarEmployee(Employees employees)
+        {
+            _context.Employees.Add(employees);
+            _context.SaveChanges();
+        }
+    }
+}
